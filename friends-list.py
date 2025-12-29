79d1e5da09ec50e712d25d59f263b1725f322203
sajedemehrdad <sajedemehrdad56@gmail.com>
2025-12-29 13:20:34 +0330

    add friend-list program

---
+++ friends-list.py
@@ -0,0 +1,20 @@
+friends=[]
+flag=True
+while flag:
+    name=input("write your friends name, end if finished    :   ")
+    
+    if name=="end":
+        flag=False
+    elif not name.isalpha():
+        print("error")
+    else:
+        friends.append(name)
+   
+Friend=""
+
+for index in range(0,len(friends)):
+
+    Friend="+".join(friends)
+
+print(Friend)
+
